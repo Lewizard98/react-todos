9514e33f75426061f3a516b3cdb9361ab8186ba0
Lewizard98 <lewizkeech01@gmail.com>
2020-06-23 14:00:55 +0100

    Completed Todo App v1.0

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <footer>
+            <hr/>
+            <h3>An App by Lewis Keech</h3>
+            <hr/>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <hr/>
+            <h1>ToDo4U</h1>
+            <h4>A simple todo app</h4>
+            <hr/>
+        </header>
+    )
+}
+
+export default Header
